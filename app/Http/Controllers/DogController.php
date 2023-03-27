099193a0849f24760dcd863639b1570e08f13642
Debashis Dip <nihan.dip@gmail.com>
2023-03-27 21:47:17 +0200

    ADD: Meilisearch + pagination for list route

--- app/Http/Controllers/DogController.php
+++ app/Http/Controllers/DogController.php
@@ -2,32 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Dog;
 use Illuminate\Http\Request;
 
 class DogController extends Controller
 {
     public function addDog(Request $request)
     {
+        # load the json data from the request
+        $dogs = $request->json()->all();
+        if (!$this->validate_dog_has_name($dogs)) {
+            return response()->json([
+                'message' => 'Dog must have a name'
+            ], 400);
+        }
+
+        // insert the data into the database
+        $formatted_data = [];
+        foreach ($dogs as $dog) {
+            $formatted_data[] = [
+                'data' => json_encode($dog),
+                'created_at' => now(),
+            ];
+        }
+
+        Dog::insert($formatted_data);
+
         return response()->json([
             'message' => 'Dog added successfully',
-            'dog' => $request->all()
+            'dog' => $dogs
         ]);
     }
 
-    public function getDogs(Request $request) {
-        return response()->json([
-            'dogs' => [
-                [
-                    'name' => 'Fido',
-                    'breed' => 'Labrador',
-                    'age' => 3
-                ],
-                [
-                    'name' => 'Spot',
-                    'breed' => 'Poodle',
-                    'age' => 5
-                ]
-            ]
-        ]);
+    public function getDogs(Request $request)
+    {
+        // take the search query from the request
+        $search_query = $request->query('search');
+        $dogs = Dog::search($search_query)->paginate(30);
+        return response()->json(
+            $dogs
+        );
+    }
+
+    private function validate_dog_has_name($dogs): bool
+    {
+        foreach ($dogs as $dog) {
+            if (!isset($dog['name'])) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- app/Models/Dog.php
+++ app/Models/Dog.php
@@ -4,8 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Laravel\Scout\Searchable;
 
 class Dog extends Model
 {
     use HasFactory;
+    use Searchable;
+    protected $fillable = [
+        'data'
+    ];
+
+    protected function data(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => json_decode($value, true),
+            set: fn ($value) => json_encode($value),
+        );
+    }
+
+    public function searchableAs()
+    {
+        return 'dogs_index';
+    }
+
+    public function toSearchableArray()
+    {
+        $array = $this->toArray();
+
+        // extract json data from the data column and add it as a top level key
+        $array['name'] = $this->data['name'];
+
+        // remove the data column
+        unset($array['data']);
+
+        return $array;
+    }
 }
